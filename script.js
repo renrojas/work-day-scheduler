dca27785b8bc948a27acebe9e5159d0822239a2b
renrojas <karensadithrojas@gmail.com>
2023-09-11 20:11:20 -0600

    added the rest of the 9-5 time slots

--- script.js
+++ script.js
@@ -40,6 +40,14 @@ function updater () {
 updater();
 
 $("#hour-9 .description").val(localStorage.getItem("hour-9"));
+$("#hour-10 .description").val(localStorage.getItem("hour-10"));
+$("#hour-11 .description").val(localStorage.getItem("hour-11"));
+$("#hour-12 .description").val(localStorage.getItem("hour-12"));
+$("#hour-13 .description").val(localStorage.getItem("hour-13"));
+$("#hour-14 .description").val(localStorage.getItem("hour-14"));
+$("#hour-15 .description").val(localStorage.getItem("hour-15"));
+$("#hour-16 .description").val(localStorage.getItem("hour-16"));
+$("#hour-17 .description").val(localStorage.getItem("hour-17"));
 
   // TODO: Add code to apply the past, present, or future class to each time
   // block by comparing the id to the current hour. HINTS: How can the id
@@ -51,7 +59,7 @@ $("#hour-9 .description").val(localStorage.getItem("hour-9"));
   // the values of the corresponding textarea elements. HINT: How can the id
   // attribute of each time-block be used to do this?
   //
-  // TODO: Add code to display the current date in the header of the page.
-$('#currentDay').text(dayjs().format('MMMM,DD,YYYY'));
+  // Displas the current date in the header of the page
+$('#currentDay').text(dayjs().format('MMMM DD, YYYY'));
 
 });
